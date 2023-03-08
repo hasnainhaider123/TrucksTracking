0162041112c1dd160e05997c23fa48148e96af45
unknown <utbahirfan07@gmail.com>
2023-03-08 14:04:09 +0500

    Test Commit By Feature Utbah

--- app/src/main/java/com/rco/truckstracking/MainActivity.kt
+++ app/src/main/java/com/rco/truckstracking/MainActivity.kt
@@ -2,13 +2,26 @@ package com.rco.truckstracking
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val showButton
+                = findViewById<Button>(R.id.showInput)
+        val editText
+                = findViewById<EditText>(R.id.editText)
+        val textView
+                = findViewById<TextView>(R.id.txtVw)
+
     }
 
     //utbah first commit changes
+
 
 }
