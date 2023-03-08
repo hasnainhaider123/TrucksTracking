9b2f71a3d5ea7eb3c7e6fecc36016a80a56ab212
Hasnain <mr.hasnainhaider@gmail.com>
2023-03-08 14:02:43 +0500

    Test Changes feature-hasnain

--- app/src/main/java/com/rco/truckstracking/MainActivity.kt
+++ app/src/main/java/com/rco/truckstracking/MainActivity.kt
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    private val TAG = MainActivity::class.java.simpleName
+
+    lateinit var splashThread: Thread
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
